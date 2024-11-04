1d1554f011e45966c215151373719fe88c094528
Maksym Demkiv <maxymdemkiv@gmail.com>
2024-11-04 20:53:40 +0200

    final preparations

--- game_reviews/game_reviews/settings.py
+++ game_reviews/game_reviews/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
